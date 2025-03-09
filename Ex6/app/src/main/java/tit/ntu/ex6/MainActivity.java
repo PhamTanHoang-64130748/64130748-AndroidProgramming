fc0216d77bd0290604cea4164e073a6d6d0e15e6
PhamTanHoang-64130748 <hoang.pt.64cntt@ntu.edu.vn>
2025-03-09 19:34:54 +0700

    Ex6_IntentDonGian

--- Ex6/app/src/main/java/tit/ntu/ex6/MainActivity.java
+++ Ex6/app/src/main/java/tit/ntu/ex6/MainActivity.java
@@ -1,6 +1,9 @@
 package tit.ntu.ex6;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button nutMH2;
+    Button nutMH3;
+    void TimDieuKHien(){
+        nutMH2 = (Button) findViewById(R.id.btnMH2);
+        nutMH3 = (Button) findViewById(R.id.btnMH3);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        TimDieuKHien();
+        nutMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMH2 = new Intent(MainActivity.this, MH2Activity.class);
+                startActivity(intentMH2);
+            }
+        });
+        nutMH3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMH3 = new Intent(MainActivity.this, MH2Activity.class);
+                startActivity(intentMH3);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
