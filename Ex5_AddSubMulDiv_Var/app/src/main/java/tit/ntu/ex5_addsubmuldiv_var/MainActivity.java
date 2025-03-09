22db3c7774891ad91d82030e869ab5acb0f10601
PhamTanHoang-64130748 <hoang.pt.64cntt@ntu.edu.vn>
2025-03-09 19:01:45 +0700

    Ex5_AddSubMulDiv_Var
    xong bài

--- Ex5_AddSubMulDiv_Var/app/src/main/java/tit/ntu/ex5_addsubmuldiv_var/MainActivity.java
+++ Ex5_AddSubMulDiv_Var/app/src/main/java/tit/ntu/ex5_addsubmuldiv_var/MainActivity.java
@@ -1,6 +1,10 @@
 package tit.ntu.ex5_addsubmuldiv_var;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtSoA;
+    EditText edtSoB;
+    Button btnCong, btnTru, btnNhan, btnChia;
+    TextView tvKetQua;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,82 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TimView();
+        btnCong.setOnClickListener(boLangNghe_XuLyCong);
+        btnTru.setOnClickListener(boLangNghe_XuLyTru);
+        btnNhan.setOnClickListener(boLangNghe_XuLyNhan);
+        btnChia.setOnClickListener(boLangNghe_XuLyChia);
+    }
+
+    View.OnClickListener boLangNghe_XuLyCong = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String strSo1 = edtSoA.getText().toString();
+            String strSo2 = edtSoB.getText().toString();
+
+            double soA = Double.parseDouble(strSo1);
+            double soB = Double.parseDouble(strSo2);
+
+            double tong = soA + soB;
+
+            String strKQ = String.valueOf(tong);
+            tvKetQua.setText(strKQ);
+
+        }
+    };
+
+    View.OnClickListener boLangNghe_XuLyTru = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String strSo1 = edtSoA.getText().toString();
+            String strSo2 = edtSoB.getText().toString();
+
+            double soA = Double.parseDouble(strSo1);
+            double soB = Double.parseDouble(strSo2);
+
+            double tong = soA - soB;
+
+            String strKQ = String.valueOf(tong);
+            tvKetQua.setText(strKQ);
+
+        }
+    };
+
+    View.OnClickListener boLangNghe_XuLyNhan = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String strSo1 = edtSoA.getText().toString();
+            String strSo2 = edtSoB.getText().toString();
+
+            double soA = Double.parseDouble(strSo1);
+            double soB = Double.parseDouble(strSo2);
+
+            double tong = soA * soB;
+
+            String strKQ = String.valueOf(tong);
+            tvKetQua.setText(strKQ);
+        }
+    };
+
+    View.OnClickListener boLangNghe_XuLyChia = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String strSo1 = edtSoA.getText().toString();
+            String strSo2 = edtSoB.getText().toString();
+            double soA = Double.parseDouble(strSo1);
+            double soB = Double.parseDouble(strSo2);
+            double tong = soA / soB;
+            String strKQ = String.valueOf(tong);
+            tvKetQua.setText(strKQ);
+        }
+    };
+    void TimView(){
+        edtSoA = (EditText) findViewById(R.id.edtSoA);
+        edtSoB = (EditText) findViewById(R.id.edtSoB);
+        btnCong = (Button) findViewById(R.id.btnCong);
+        btnTru = (Button) findViewById(R.id.btnTru);
+        btnNhan = (Button) findViewById(R.id.btnNhan);
+        btnChia = (Button) findViewById(R.id.btnChia);
+        tvKetQua = (TextView) findViewById(R.id.tvKetQua);
     }
 }
