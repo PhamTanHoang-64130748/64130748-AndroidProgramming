31c20b7c02ed25ce36d71f11a9eb61872f855bb8
PhamTanHoang-64130748 <hoang.pt.64cntt@ntu.edu.vn>
2025-04-23 20:07:51 +0700

    Bottom Navigation
    Xong bài

--- BottomNavigation/app/src/main/java/tit/ntu/bottomnavigation/MainActivity.java
+++ BottomNavigation/app/src/main/java/tit/ntu/bottomnavigation/MainActivity.java
@@ -1,12 +1,17 @@
 package tit.ntu.bottomnavigation;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +20,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        BottomNavigationView bottomNav;
+        bottomNav = findViewById(R.id.bot_nav);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int mnuItemDuocChonId = item.getItemId();
+                if (mnuItemDuocChonId ==R.id.mnu_home){
+                    Toast.makeText(MainActivity.this , "Thay HOME", Toast.LENGTH_LONG).show();}
+                else if (mnuItemDuocChonId == R.id.mnu_search){
+                    Toast.makeText(MainActivity.this , "Thay SEARCH", Toast.LENGTH_LONG).show();}
+                else if (mnuItemDuocChonId == R.id.mnu_profile){
+                    Toast.makeText(MainActivity.this , "Thay PROFILE", Toast.LENGTH_LONG).show();}
+                else
+                    return false;
+                return  true;
+            }
         });
     }
 }
